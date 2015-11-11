ab3dc17bcb7cfdf6436d6112ad2cb492e63a3f59
Cynrick Chiu <cyn.2k5@outlook.com>
2015-11-11 12:30:02 +0900

    Add style for chat page

--- public/js/app.js
+++ public/js/app.js
@@ -15,10 +15,12 @@ socket.on('connect', function() {
 
 socket.on('message', function(message) {
   var momentTimestamp = moment.utc(message.timestamp);
-  var $message = $('.messages');
+  var $messages = $('.messages');
+  var $message = $('<li class="list-group-item"></li>');
 
   $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
   $message.append('<p>' + message.text + '</p>');
+  $messages.append($message);
 });
 
 var $form = $('#message-form');
